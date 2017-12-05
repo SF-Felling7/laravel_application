784398d37466d3147d59c32c048801181c7c4f5a
Sean Felling <Snfelling7@gmail.com>
2017-12-05 16:39:10 -0600

    added nav bar

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -3,11 +3,24 @@
   <head>
     <meta charset="utf-8">
     <title>Laravel Portfolio</title>
-    <link rel="stylesheet" href="http://yourdomain.com/materialize-css/css/materialize.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="http://yourdomain.com/materialize-css/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
   </head>
   <body>
-    <h3>Hello Laravel!</h3>
+  <header>
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li class="category"><a href="about.blade.php">About Me</a></li>
+        <li class="category"><a href="experience.blade.php">Experience</a></li>
+        <li class="category"><a href="services.blade.php">Services</a></li>
+        <li class="category"><a href="contact.blade.php">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+</header>
   </body>
 </html>
