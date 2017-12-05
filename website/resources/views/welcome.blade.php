1ef6c2baea9601321582c9d70d2a6cf08bcf8b1b
Sean Felling <Snfelling7@gmail.com>
2017-12-05 16:06:48 -0600

    added materialize to laravel

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Laravel Portfolio</title>
+    <link rel="stylesheet" href="http://yourdomain.com/materialize-css/css/materialize.min.css">
+    <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="http://yourdomain.com/materialize-css/js/materialize.min.js"></script>
   </head>
   <body>
     <h3>Hello Laravel!</h3>
